451ab2792aa4990fc962efd2e63ce694c93483f3
MaratAslanov190913451 <marik2.70@mail.ru>
2022-12-18 01:21:29 +0300

    Create 2-4.cpp

---
+++ Algorithmes/2.4/2-4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, space, stars = 1;
+
+    cin >> n;
+
+    space = n / 2;
+
+    for (int i = 0; i < n; i += 2)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            std::cout << ' ';
+        }
+        for (int j = 0; j < stars; j++)
+        {
+            std::cout << '*';
+        }
+        space--;
+        stars += 2;
+        cout << endl;
+    }
+}
