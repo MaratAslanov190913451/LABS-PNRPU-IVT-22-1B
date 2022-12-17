a356129ca40d09bc28e9c8dfba5307ba78bc1515
MaratAslanov190913451 <marik2.70@mail.ru>
2022-12-18 01:23:18 +0300

    Create 2-7.cpp

---
+++ Algorithmes/2.7/2-7.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, length;
+
+    cin >> n;
+
+    length = sqrt(n);
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    for (int i = 0; i < n - 2; i++)
+    {
+        cout << "* ";
+        for (int j = 0; j < (n - 2) * 2; j++)
+        {
+            cout << ' ';
+        }
+        cout << "* ";
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+}
