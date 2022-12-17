9cfca9de4664121f71a0408e969ce7da5ccd67ef
MaratAslanov190913451 <marik2.70@mail.ru>
2022-12-18 01:23:38 +0300

    Create 2-8.cpp

---
+++ Algorithmes/2.8/2-8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, stars;
+
+    cin >> n;
+    stars = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < stars; j++)
+        {
+            cout << '*';
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << ' ';
+        }   
+        stars--;
+        cout << endl;
+    }
+}
