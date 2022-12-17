cb36be2015d64d679de95515381daf403da65de9
MaratAslanov190913451 <marik2.70@mail.ru>
2022-12-18 01:22:29 +0300

    Create 2-5.cpp

---
+++ Algorithmes/2.5/2-5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, space;
+     
+    cin >> n;
+    space = n;
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            std::cout << ' ';
+        }
+        for (int j = 0; j < i; j++)
+        {
+            std::cout << '*';
+        }
+        cout << endl;
+        space--;
+    }
+}
