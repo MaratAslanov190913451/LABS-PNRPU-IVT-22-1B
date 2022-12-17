28e542bfe28bfbee745511487ba6595a120597d3
MaratAslanov190913451 <marik2.70@mail.ru>
2022-12-18 01:22:50 +0300

    Create 2-6.cpp

---
+++ Algorithmes/2.6/2-6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, space = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            std::cout << ' ';
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            std::cout << '*';
+        }
+        cout << endl;
+        space++;
+    }
+}
