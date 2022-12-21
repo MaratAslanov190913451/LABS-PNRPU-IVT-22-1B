a073bfef9f65eed65237a3b5798b51b2fbec1347
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:49:10 +0500

    Add files via upload

---
+++ Algorithmes/2.15/2-15.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n, a, s;
+    bool flag = false;
+    cin >> n;
+    cin >> s;
+    while (n > 0)
+    {
+        a = n % 10;
+
+        if (a == s)
+        {
+            flag = true;
+        }
+        if (n >= 10)
+        {
+            n = n / 10;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (flag == true)
+    {
+        std::cout << "Цифра S найдена" << std::endl;
+    }
+    else
+    {
+        std::cout << "Цифра S не найдена" << std::endl;
+    }
+}
