c7f2b91775447115211ac489461fab64219e3453
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:52:13 +0500

    Add files via upload

---
+++ Algorithmes/2.19/2-19.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, sum = 0;
+    std::cout << "N:" << std::endl;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            sum -= i;
+        }
+        else
+        {
+            sum += i;
+        }
+    }
+    std::cout << sum << std::endl;
+}
