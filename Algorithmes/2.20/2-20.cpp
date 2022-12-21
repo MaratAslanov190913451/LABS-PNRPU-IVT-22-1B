53d1505bcf91c2791265e76d033e389dc3e5d3d6
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 17:39:37 +0500

    Update 2-20.cpp

--- Algorithmes/2.20/2-20.cpp
+++ Algorithmes/2.20/2-20.cpp
@@ -1,31 +1,31 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
-
+using namespace std;
 int main()
 {
-    setlocale(LC_ALL, "Rus");
-    float x, n, s, i = 1;
-    bool flag = false;
-    std::cout << "n: " << std::endl;
-    std::cin >> n;
-    std::cout << "s: " << std::endl;
-    std::cin >> s;
-    while ((i <= n) && (flag == false))
-    {
-        x = sin(n + i / n);
-        x = round(x * 10) / 10;
-        if (x == s)
-        {
-            flag = true;
-        }
-        i++;
-    }
-    if (flag == true)
-    {
-        std::cout << "s найдено" << std::endl;
-    }
-    else
-    {
-        std::cout << "s не найдено" << std::endl;
-    }
+	setlocale(LC_ALL, "RUS");
+	double n, S;
+	bool flag = false;
+
+	cout << "Введите количество элементов n" << endl;
+	cin >> n;
+	cout << "Введите искомый элемент S" << endl;
+	cin >> S;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (S == sin(n + (i / n)))
+		{
+			flag = true;
+		}
+	}
+	if (flag)
+	{
+		cout << "Элемент S есть среди других элементов последовательности" << endl;
+	}
+	else if (!flag)
+	{
+		cout << "Элемента S нет среди других элементов последовательности" << endl;
+	}
+	return 0;
 }
