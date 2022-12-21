ff469b828ba09c8b997ff69d421cebcfe188fc36
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 17:38:54 +0500

    Update 2-19.cpp

--- Algorithmes/2.19/2-19.cpp
+++ Algorithmes/2.19/2-19.cpp
@@ -1,20 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    int n, sum = 0;
-    std::cout << "N:" << std::endl;
-    std::cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        if (i % 3 == 0)
-        {
-            sum -= i;
-        }
-        else
-        {
-            sum += i;
-        }
-    }
-    std::cout << sum << std::endl;
+	setlocale(LC_ALL, "RUS");
+	int N;
+	int S = 0;
+	
+	cout << "Введите количество слагаемых N" << endl;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (i % 3 == 0)
+		{
+			S -= i;
+		}
+		else
+		{
+			S += i;
+		}
+			
+	}
+	cout << "Сумма S равна:" << S << endl;
+
+	return 0;
 }
