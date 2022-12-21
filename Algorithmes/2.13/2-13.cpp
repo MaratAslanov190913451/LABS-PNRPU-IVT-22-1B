46e35bd0f46ac146b763b5eb17f1441811a8d410
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:46:11 +0500

    Add files via upload

---
+++ Algorithmes/2.13/2-13.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float a, b, c, x1 = 0, x2 = 0, x = 0, d;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    cout << "c: ";
+    cin >> c;
+    d = b * b - (4 * a * c);
+    if (d > 0)
+    {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        cout << "x1: " << x1 << endl;
+        cout << "x2: " << x2 << endl;
+    }
+    else if (d == 0)
+    {
+        x = ((-1) * b) / 2 * a;
+        cout << "x: " << x1 << endl;
+    }
+    else
+    {
+        cout << "Корней нет" << endl;
+    }
+}
