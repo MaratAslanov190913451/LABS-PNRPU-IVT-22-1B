5151b286ab64612b31b6089b747981dabbc37db3
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 17:25:36 +0500

    Update 2-13.cpp

--- Algorithmes/2.13/2-13.cpp
+++ Algorithmes/2.13/2-13.cpp
@@ -1,27 +1,28 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-    setlocale(LC_ALL, "Rus");
-    float a, b, c, x1 = 0, x2 = 0, x = 0, d;
-    cout << "a: ";
-    cin >> a;
-    cout << "b: ";
-    cin >> b;
-    cout << "c: ";
-    cin >> c;
-    d = b * b - (4 * a * c);
+    setlocale(LC_ALL, "RUS");
+
+    float a, b, c, d, x1, x2;
+
+    cin >> a >> b >> c;
+    d = pow(b, 2) - 4 * a * c;
+
     if (d > 0)
     {
         x1 = (-b + sqrt(d)) / (2 * a);
         x2 = (-b - sqrt(d)) / (2 * a);
-        cout << "x1: " << x1 << endl;
-        cout << "x2: " << x2 << endl;
+        cout << "x1=" << x1 << endl;
+        cout << "x2=" << x2 << endl;
     }
     else if (d == 0)
     {
-        x = ((-1) * b) / 2 * a;
-        cout << "x: " << x1 << endl;
+        x1 = (-b) / (2 * a);
+        cout << "x1=x2=" << x1 << endl;
     }
     else
     {
