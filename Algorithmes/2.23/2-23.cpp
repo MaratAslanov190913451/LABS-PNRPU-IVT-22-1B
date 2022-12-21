507f6c4dfda924e22069ce8df2d6355d46cd26b9
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:54:26 +0500

    Add files via upload

---
+++ Algorithmes/2.23/2-23.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float x, n, max, count = 1, num = 0;
+    std::cin >> n;
+    max = sin(n + 1 / n);
+    std::cout << max << std::endl;
+    for (int i = 2; i <= n; i++)
+    {
+        x = sin(n + i / n);
+        std::cout << x << std::endl;
+        if (x > max)
+        {
+            max = x;
+            count = i;
+        }
+        else if (x == max)
+        {
+            num++;
+        }
+    }
+    std::cout << "Максимальный элемент: " << max << std::endl;
+    std::cout << "Его номер: " << count << std::endl;
+    std::cout << "Число элементов с этим значением: " << num << std::endl;
+}
