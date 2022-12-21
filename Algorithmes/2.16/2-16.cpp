bdd8712996b93cd50ce0d3b688c9e62e928e14ba
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:49:53 +0500

    Add files via upload

---
+++ Algorithmes/2.16/2-16.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int sum = 0, n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if ((i % 2 == 1) or (i == 1))
+        {
+            sum += (-1) * i;
+        }
+        else
+        {
+            sum += i;
+        }
+    }
+    cout << "Сумма S: " << sum << endl;
+}
