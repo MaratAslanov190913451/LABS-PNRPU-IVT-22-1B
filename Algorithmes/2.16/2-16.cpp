9b20917982a01288b271c8809e010478c7bab831
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 17:29:05 +0500

    Update 2-16.cpp

--- Algorithmes/2.16/2-16.cpp
+++ Algorithmes/2.16/2-16.cpp
@@ -1,20 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-    setlocale(LC_ALL, "Rus");
-    int sum = 0, n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        if ((i % 2 == 1) or (i == 1))
-        {
-            sum += (-1) * i;
-        }
-        else
-        {
-            sum += i;
-        }
-    }
-    cout << "Сумма S: " << sum << endl;
+	setlocale(LC_ALL, "RUS");
+	int N; 
+	int S = 0;
+	
+	cout << "Введите количество слагаемых N" << endl;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (i % 2 == 1)
+		{
+			S -= i;
+		}
+		else
+		{
+			S += i;
+		}
+	}
+
+	cout << S << endl;
+
+	return 0;
 }
