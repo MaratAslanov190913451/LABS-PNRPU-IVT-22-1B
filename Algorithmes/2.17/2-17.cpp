cfba383d21b5011eb3d36b556d34d1d182277816
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:50:33 +0500

    Add files via upload

---
+++ Algorithmes/2.17/2-17.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n;
+    float x, fact, sum;
+    cin >> n;
+    cin >> x;
+    sum = 1 + x;
+    fact = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fact *= i;
+        sum += (pow(x, i) / fact);
+    }
+    cout << sum << endl;
+}
