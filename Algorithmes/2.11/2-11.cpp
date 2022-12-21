a424d6ebe1e4d22fe19b538d85009e07432ff99c
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 17:19:41 +0500

    Update 2-11.cpp

--- Algorithmes/2.11/2-11.cpp
+++ Algorithmes/2.11/2-11.cpp
@@ -3,18 +3,18 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    int n, tmp;
+    int n, num;
     bool plusFirst = false; // flag
     bool minusFirst = false; // flag
     cout << "Введите длину последовательности:" << endl;
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        cin >> tmp;
+        cin >> num;
         if (!plusFirst && !minusFirst)
         {
-            if (tmp > 0) { plusFirst = true; }
-            else if (tmp < 0) { minusFirst = true; }
+            if (num > 0) { plusFirst = true; }
+            else if (num < 0) { minusFirst = true; }
         }
     }
     if (plusFirst)
