a607da46fcbc1ce4f56d31fb144b0c1deea4804b
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:53:27 +0500

    Add files via upload

---
+++ Algorithmes/2.21/2-21.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float n, f;
+    bool flag = true;
+    std::cout << "Числа вводятся с клавиатуры до тех пор, пока не встретится число 0 (0 — признак окончания ввода). " << std::endl;
+    std::cin >> f;
+    while (f != 0)
+    {
+        std::cin >> n;
+        if (n < f && n != 0)
+        {
+            flag = false;
+        }
+        f = n;
+    }
+    if (flag)
+    {
+        std::cout << "Числа идут по возрастанию" << std::endl;
+    }
+    else
+    {
+        std::cout << "Числа не идут по возрастанию" << std::endl;
+    }
+}
