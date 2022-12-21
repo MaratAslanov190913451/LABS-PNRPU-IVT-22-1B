1909935640332bfac6d1ee541d53ce9e28cd93e5
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:44:57 +0500

    Add files via upload

---
+++ Algorithmes/2.12/2-12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int range, max, min, n;
+    cout << "Введите длину последовательности: " << endl;
+    cin >> range;
+    cout << "Введите первое число: " << endl;
+    cin >> n;
+    max = n;
+    min = n;
+    for (int i = 1; i <= range - 1; i++)
+    {
+        cout << "Введите следующее число:" << endl;
+        cin >> n;
+
+        if (n > max)
+        {
+            max = n;
+        }
+        else if (n < min)
+        {
+            min = n;
+        }
+    }
+    cout << "Сумма минимального и максимального числа: " << max + min << endl;
+}
