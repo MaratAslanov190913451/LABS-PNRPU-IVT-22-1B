c33baae3a8f324b5868649a933409c83970ec6b0
MaratAslanov190913451 <120792320+MaratAslanov190913451@users.noreply.github.com>
2022-12-21 15:52:47 +0500

    Add files via upload

---
+++ Algorithmes/2.20/2-20.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float x, n, s, i = 1;
+    bool flag = false;
+    std::cout << "n: " << std::endl;
+    std::cin >> n;
+    std::cout << "s: " << std::endl;
+    std::cin >> s;
+    while ((i <= n) && (flag == false))
+    {
+        x = sin(n + i / n);
+        x = round(x * 10) / 10;
+        if (x == s)
+        {
+            flag = true;
+        }
+        i++;
+    }
+    if (flag == true)
+    {
+        std::cout << "s найдено" << std::endl;
+    }
+    else
+    {
+        std::cout << "s не найдено" << std::endl;
+    }
+}
